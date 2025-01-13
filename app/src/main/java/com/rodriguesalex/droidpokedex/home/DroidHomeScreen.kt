b050a861ac413d30b3eabbf25e732a6f2cc55d70
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2025-01-13 07:07:49 -0300

    using state flow and enhancing the ui screen

--- app/src/main/java/com/rodriguesalex/droidpokedex/home/DroidHomeScreen.kt
+++ app/src/main/java/com/rodriguesalex/droidpokedex/home/DroidHomeScreen.kt
@@ -1,31 +1,39 @@
 package com.rodriguesalex.droidpokedex.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import com.rodriguesalex.droidpokedex.home.viewmodel.DroidHomeViewModel
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.rodriguesalex.droidpokedex.designsystem.components.DroidPokeHeader
 import com.rodriguesalex.droidpokedex.designsystem.tokens.Colors
 import com.rodriguesalex.droidpokedex.designsystem.tokens.spacing
 import com.rodriguesalex.droidpokedex.home.components.DroidHomeCell
+import com.rodriguesalex.droidpokedex.home.viewmodel.DroidHomeViewModel
+import com.rodriguesalex.droidpokedex.home.viewmodel.DroidHomeViewState
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun DroidHomeScreen(
     viewModel: DroidHomeViewModel = hiltViewModel()
 ) {
 
-    val homePageLiveData by viewModel.homePageLiveData.observeAsState()
+    val homePageState by viewModel.homeStateFlow.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -42,24 +50,75 @@ internal fun DroidHomeScreen(
                 modifier = Modifier.padding(spacing.medium.dp)
             )
 
-            homePageLiveData?.let {
-                LazyColumn(
-                ) {
-                    it.forEach { pokemon ->
-                        item {
+            when (homePageState) {
+                is DroidHomeViewState.Error -> {
+                    // TODO: Implement error state
+                }
+
+                is DroidHomeViewState.Loading -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                    ) {
+                        LoadingIndicator(
+                            modifier = Modifier.align(Center)
+                        )
+                    }
+                }
+
+                is DroidHomeViewState.Success -> {
+                    val pokemons = (homePageState as DroidHomeViewState.Success).pokemons
+
+                    LazyColumn {
+                        items(pokemons.size) { index ->
+                            val pokemon = pokemons[index]
                             DroidHomeCell(
                                 pokemonName = pokemon.name,
                                 pokemonNumber = pokemon.id,
                                 pokemonImageUrl = pokemon.pokemonImageUrl,
-                                types = pokemon.types.map {
-                                    it.type.name
-                                },
+                                types = pokemon.types.map { it.type.name },
                                 backgroundColor = pokemon.backgroundColor
                             )
+
+                            if (index >= pokemons.size - 3 && !isLoading) {
+                                viewModel.loadMorePokemons()
+                            }
+                        }
+
+                        if (isLoading) {
+                            item {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = spacing.medium.dp)
+                                ) {
+                                    CircularProgressIndicator(
+                                        color = Color.White,
+                                        modifier = Modifier.align(Center)
+                                    )
+                                }
+                            }
                         }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun LoadingIndicator(
+    modifier: Modifier
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(spacing.medium.dp),
+    ) {
+        CircularProgressIndicator(
+            color = Color.White,
+            modifier = modifier
+        )
     }
 }
--- app/src/main/java/com/rodriguesalex/droidpokedex/home/viewmodel/DroidHomeViewModel.kt
+++ app/src/main/java/com/rodriguesalex/droidpokedex/home/viewmodel/DroidHomeViewModel.kt
@@ -1,12 +1,13 @@
 package com.rodriguesalex.droidpokedex.home.viewmodel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rodriguesalex.domain.model.PokemonListItem
 import com.rodriguesalex.domain.usecase.GetPokeHomeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 @HiltViewModel
@@ -14,14 +15,44 @@ class DroidHomeViewModel @Inject constructor(
     private val getHomeUseCase: GetPokeHomeUseCase
 )  : ViewModel() {
 
-    // TODO: use stateflow instead
-    val homePageLiveData = MutableLiveData<List<PokemonListItem>>()
+    private val _homeStateFlow = MutableStateFlow<DroidHomeViewState>(DroidHomeViewState.Loading)
+    val homeStateFlow = _homeStateFlow
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private var currentPage = 0
+    private val pageSize = 20
 
     init {
-        viewModelScope.launch {
-            val pokemons = getHomeUseCase.invoke(GetPokeHomeUseCase.Params(150, 0))
+        loadMorePokemons()
+    }
 
-            homePageLiveData.value = pokemons.results
+    fun loadMorePokemons() {
+        if (_isLoading.value) return
+
+        _isLoading.value = true
+        viewModelScope.launch {
+            runCatching {
+                getHomeUseCase.invoke(
+                    GetPokeHomeUseCase.Params(limit = pageSize, offset = currentPage * pageSize)
+                ).results
+            }.onSuccess { newPokemons ->
+                _homeStateFlow.updateStateWith(newPokemons)
+                currentPage++
+            }.onFailure { throwable ->
+                _homeStateFlow.value = DroidHomeViewState.Error
+            }
+            _isLoading.value = false
         }
+    }
+}
+
+private fun MutableStateFlow<DroidHomeViewState>.updateStateWith(newPokemons: List<PokemonListItem>) {
+    value = when (val currentState = value) {
+        is DroidHomeViewState.Success -> {
+            DroidHomeViewState.Success(currentState.pokemons + newPokemons)
+        }
+        else -> DroidHomeViewState.Success(newPokemons)
     }
 }
