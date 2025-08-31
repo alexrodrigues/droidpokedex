0df5d8cf0172b63ab0d2e37db2228774708aa316
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2025-08-31 16:50:25 -0300

    Implement retry functionality and DroidSecondaryButton
    - Added a `retry` string resource.
    - Introduced `DroidSecondaryButton.kt` with its preview.
    - Updated `Colors.kt` with `white700` and `white`.
    - Modified `DroidErrorComponent.kt` to include the `DroidSecondaryButton` for retrying and adjusted styling.
    - Changed the `containerColor` in `DroidPrimaryButton.kt` to `Colors.red700`.
    - Integrated the retry logic in `DroidHomeScreen.kt` by calling `viewModel.onRetry()`.
    - Added `onRetry()` function in `DroidHomeViewModel.kt` to reset state and load more Pokemons.
    - Updated `deploymentTargetSelector.xml` to include `DroidSecondaryButtonPreview`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.rodriguesalex.droidpokedex"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.rodriguesalex.droidpokedex"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- home/data/build.gradle.kts
+++ home/data/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.rodriguesalex.droidpokedex.home.data"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
